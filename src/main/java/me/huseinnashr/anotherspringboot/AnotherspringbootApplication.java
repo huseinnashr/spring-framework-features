5579c73e430c86286b6bd2fc430c8c203eaef823
Husein <nvdhunter007@gmail.com>
2020-06-18 19:46:04 +0700

    init boot project

---
+++ src/main/java/me/huseinnashr/anotherspringboot/AnotherspringbootApplication.java
@@ -0,0 +1,13 @@
+package me.huseinnashr.anotherspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/huseinnashr/anotherspringboot/AnotherspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.huseinnashr.anotherspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
